a2deface9500ed23683bc9a1ffcf8ad2a839f82e
Jonathan Koff <jkoff@Jonathans-MacBook-Pro.local>
2014-03-01 18:09:38 -0500

    I dont remember whats in this

--- app/models/donee.rb
+++ app/models/donee.rb
@@ -1,9 +1,9 @@
 class Donee < ActiveRecord::Base
-  def self.search_by_name(name, n, start=nil)
-    unless start.nil?
-      where{(name =~ "%#{name}%") & (id >= start)}.limit(n+1)
+  def self.search_by_name(in_name, in_n, in_start=nil)
+    unless in_start.nil?
+      where{(name =~ "%#{in_name}%") & (id >= in_start)}.limit(in_n+1)
     else
-      where{(name =~ "%#{name}%")}.first
+      where{name =~ "%#{in_name}%"}.limit(in_n+1)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Clapp::Application.routes.draw do
   end
 
   scope :api do
+  	#scope :charity do
+  	#	get ':bn'    => 'charity#search', :as => 'api_charity_search'
+  	#	get 'search' => 'charity#search', :as => 'api_charity_search'
+  	#end
     get 'search_by_name' => 'api#search_by_name', :as => 'api_search_by_name'
     get 'get_chart'      => 'api#get_chart',      :as => 'api_get_chart'
   end
