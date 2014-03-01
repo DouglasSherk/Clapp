61572fff93b100d0539a665211341cacd2d0601f
DouglasSherk <github@sherk.me>
2014-03-01 14:56:47 -0500

    DB migrations for importing donee CSV files.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,9 @@
 class ApiController < ApplicationController
   def search_by_name
-    msg = { :status => :ok }
+    start = params[:start]
+    n = params[:count] || 5
+    res = Donee.search_by_name(params[:name], n, start)
+    msg = { :status => :ok, :results => res[0,n], :next => res[n] ? res[n]["id"] : nil }
     render :json => msg
   end
 end
---
+++ db/migrate/20140301044300_create_donees.rb
@@ -0,0 +1,27 @@
+require 'CSV'
+
+class CreateDonees < ActiveRecord::Migration
+  def up
+    create_table :donees do |t|
+      t.string  :bn1
+      t.string  :fpe
+      t.integer :n
+      t.string  :bn2
+      t.string  :name
+      t.string  :associated
+      t.string  :city
+      t.string  :province
+      t.decimal :totalgifts, :precision => 14, :scale => 2
+      t.decimal :giftsinkind, :precision => 14, :scale => 2
+      t.decimal :designatedgifts, :precision => 14, :scale => 2
+    end
+    fields = 'bn1,fpe,n,bn2,name,associated,city,province,totalgifts,giftsinkind,designatedgifts'.split(',')
+    CSV.foreach('db/donee.csv', {:encoding=>'windows-1251'}) do |row|
+      Donee.create(Hash[fields.zip(row)])
+    end
+  end
+
+  def down
+    drop_table :donees
+  end
+end
