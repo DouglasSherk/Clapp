0c8bf3033725c47d6b60fdf033e10f3939d18a27
DouglasSherk <github@sherk.me>
2014-03-01 15:42:38 -0500

    Stuff.

--- app/models/donee.rb
+++ app/models/donee.rb
@@ -1,5 +1,5 @@
 class Donee < ActiveRecord::Base
-	def self.search_by_name(name, n, start=nil)
-		where('name LIKE ? AND (? is NULL OR id >= ?)', '%'+name+'%', start, start).limit(n+1)
-	end
+  def self.search_by_name(name, n, start=nil)
+    where('name LIKE ? AND (? is NULL OR id >= ?)', '%'+name+'%', start, start).limit(n+1)
+  end
 end
