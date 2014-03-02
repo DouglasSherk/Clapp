0cf86fd0855e096c3dcd4a36c48dc8624dff4342
Parth <ash.parth.dave@gmail.com>
2014-03-01 22:32:00 -0500

    remove chart route

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Clapp::Application.routes.draw do
     scope :charity do
       get 'search' => 'api/charity#search', :as => 'api_charity_search'
       get ':bn'    => 'api/charity#show',   :as => 'api_charity_show'
-      get 'get_chart' => 'api/charity#chart', :as => 'api_charity_get_chart'
     end
   end
 
