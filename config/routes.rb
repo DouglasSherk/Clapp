02796cdaa4fb2703be9ffea23f64ca0f1929faad
Matthew Miner <matthew@matthewminer.com>
2014-03-02 00:26:07 -0500

    Wire up charity tab-to-tab navigation.

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Clapp::Application.routes.draw do
     get 'causes' => 'home#index', :as => 'causes'
     get 'recommended' => 'home#index', :as => 'recommended'
     get 'settings' => 'home#index', :as => 'settings'
-    get 'charity/:id/financials' => 'home#index', :as => 'charity_financials'
-    get 'charity/:id/description' => 'home#index', :as => 'charity_description'
-    get 'charity/:id/news' => 'home#index', :as => 'charity_news'
+    get 'charity/:bn/financials' => 'home#index', :as => 'charity_financials'
+    get 'charity/:bn/description' => 'home#index', :as => 'charity_description'
+    get 'charity/:bn/news' => 'home#index', :as => 'charity_news'
   end
 
   scope :api do
