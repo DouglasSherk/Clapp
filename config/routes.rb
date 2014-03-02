564a1275dc2dc46f9b2acbdfb5081c0d299a84cd
DouglasSherk <github@sherk.me>
2014-03-01 20:53:36 -0500

    Fix up broken API paths.

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Clapp::Application.routes.draw do
     get '' => 'home#index', :as => 'templates'
   end
 
-  namespace :api do
-  	scope :charity do
-  		get 'search' => 'api/charity#search', :as => 'api_charity_search'
-  		get ':bn'    => 'api/charity#show',   :as => 'api_charity_show'
-  	end
+  scope :api do
+    scope :charity do
+      get 'search' => 'api/charity#search', :as => 'api_charity_search'
+      get ':bn'    => 'api/charity#show',   :as => 'api_charity_show'
+    end
+
     get 'get_chart' => 'api#get_chart', :as => 'api_get_chart'
   end
 
