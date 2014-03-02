bab5a64f2583382186c64ef9ace318ac7455ef77
Parth <ash.parth.dave@gmail.com>
2014-03-01 19:41:47 -0500

    Chart test

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Clapp::Application.routes.draw do
     get '' => 'home#index', :as => 'templates'
   end
 
-  scope :api do
+  namespace :api do
   	scope :charity do
   		get 'search' => 'api/charity#search', :as => 'api_charity_search'
   		get ':bn'    => 'api/charity#show',   :as => 'api_charity_show'
   	end
-    get 'get_chart'      => 'api#get_chart',      :as => 'api_get_chart'
+    get 'get_chart' => 'api#get_chart', :as => 'api_get_chart'
   end
 
   root 'home#index'
